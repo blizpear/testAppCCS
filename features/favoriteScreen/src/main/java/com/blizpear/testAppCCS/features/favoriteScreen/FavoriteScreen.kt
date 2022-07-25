7c741ccc3c982f66afd5f7ed581b0fb3e77bdd9e
blizpear <blizpear@gmail.com>
2022-07-25 17:58:40 +0700

    create fragment

--- features/favoriteScreen/src/main/java/com/blizpear/testAppCCS/features/favoriteScreen/FavoriteScreen.kt
+++ features/favoriteScreen/src/main/java/com/blizpear/testAppCCS/features/favoriteScreen/FavoriteScreen.kt
@@ -1,5 +1,6 @@
 package com.blizpear.testAppCCS.features.favoriteScreen
 
+import com.blizpear.testAppCCS.features.favoriteScreen.ui.FavoriteScreenFragment
 import com.github.terrakok.cicerone.androidx.FragmentScreen
 
 fun getFavoriteScreen() = FragmentScreen { FavoriteScreenFragment.newInstance() }
